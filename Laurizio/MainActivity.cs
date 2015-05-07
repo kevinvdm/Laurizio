f28d07909b4d543d29fb90d319539c44de77dd04
Muldoser <michielmldr@gmail.com>
2015-05-07 17:05:08 +0200

    Update

--- Laurizio/MainActivity.cs
+++ Laurizio/MainActivity.cs
@@ -4,8 +4,9 @@ using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
-using Android.Widget;
+using Android.Widget;	
 using Android.OS;
+using System.Collections.Generic;
 
 namespace Laurizio
 {
@@ -13,12 +14,27 @@ namespace Laurizio
 	public class MainActivity : Activity
 	{
 
+		List<string> mSettingsItems = new List<string>();
+		ArrayAdapter mSettingsAdapter;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+
+			//var myHomePage = new contentPage();
+
+
+	//DATABASE ATTEMPT
+			//Valid filepath for dbfile to be stored
+//			string dbPath = System.IO.Path.Combine (System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal), "database.db3");
+//			var sqliteFilename = "MyDatabase.db3";
+//			string libraryPath = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal);
+//
+//			object locker = new object ();
+//
 
 		}
 	}
--- Laurizio/Resources/Resource.designer.cs
+++ Laurizio/Resources/Resource.designer.cs
@@ -60,20 +60,26 @@ namespace Laurizio
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int calendarView1 = 2131034113;
-			
 			// aapt resource value: 0x7f050002
-			public const int editText1 = 2131034114;
+			public const int calendarView1 = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int frameContent = 2131034113;
+			
+			// aapt resource value: 0x7f050006
+			public const int leftListView = 2131034118;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
 			
 			// aapt resource value: 0x7f050003
-			public const int textView2 = 2131034115;
+			public const int notes = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int textView3 = 2131034116;
+			public const int textView2 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView3 = 2131034117;
 			
 			static Id()
 			{
